f8d8747e6c98345cdf3961c9810ea6c2ade6522a
EthUls <guillaume.darlin7@yahoo.com>
2023-05-04 11:31:31 +0200

    feat: Possibilité de répondre aux questions

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Question;
+use App\Form\CommentType;
 use App\Form\QuestionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -38,9 +40,26 @@ class QuestionController extends AbstractController
         return $this->render('question/index.html.twig', ['form' => $formQuestion->createView()]);
     }
     #[Route('/question/{id}', name: 'show_question')]
-    public function show(Request $request, Question $question) {
-    
+    public function show(Request $request, Question $question, EntityManagerInterface $em) {
 
-        return $this->render('question/show.html.twig', ['question' => $question]);
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+
+            $comment->setCreatedAt(new \DateTimeImmutable());
+            $comment->setRating(0);
+            $comment->setQuestion($question);
+
+            $em->persist($comment);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre réponse a bien été publiée');
+            
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render('question/show.html.twig', ['question' => $question, 'form' => $commentForm->createView()]);
     }
 }
